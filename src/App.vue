744df3a06db199f4ddacf7588ab6ce8327fafd00
qinqiang <qinqiang@shoudaozi.com>
2021-02-20 11:32:50 +0800

    免费的配额好少，慎用慎用

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,11 @@ export default {
   },
   async created(){
     // ***云数据库***
-    const app = this.$cloudbase;
-    await app.auth().anonymousAuthProvider().signIn();
-    const db = app.database();
-    const {data} = await db.collection('all_goods').where({title:'将进酒'}).get();
-
-    console.log('云数据库,返回 --> ', data);
+    // const app = this.$cloudbase;
+    // await app.auth().anonymousAuthProvider().signIn();
+    // const db = app.database();
+    // const {data} = await db.collection('all_goods').where({title:'将进酒'}).get();
+    // console.log('云数据库,返回 --> ', data);
   },
 };
 </script>
