580ba23d057ffadbbcb86149536014c08e714a3e
qinqiang <qinqiang@shoudaozi.com>
2021-02-20 10:47:53 +0800

    云数据库

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,15 @@ export default {
   components: {
     HelloWorld,
   },
+  async created(){
+    // ***云数据库***
+    const app = this.$cloudbase;
+    await app.auth().anonymousAuthProvider().signIn();
+    const db = app.database();
+    const {data} = await db.collection('all_goods').where({title:'将进酒'}).get();
+
+    console.log('云数据库,返回 --> ', data);
+  },
 };
 </script>
 
