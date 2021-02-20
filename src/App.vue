8a7c83de57ee27d80c43ad47dbe430c558ea84a5
qinqiang <qinqiang@shoudaozi.com>
2021-02-20 10:10:46 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img class="logo" alt="CloudBase logo" src="./assets/cloudbase.png" />
+    <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="欢迎使用云开发 CloudBase Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo {
+  width: 120px;
+  padding: 10px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import App from "./App.vue";
+import Cloudbase from "@cloudbase/vue-provider";
+import config from "../cloudbaserc";
+
+Vue.config.productionTip = false;
+
+console.log(process,config)
+Vue.use(Cloudbase, {
+  env: process.env.VUE_APP_ENV_ID || config.envId,
+});
+
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
