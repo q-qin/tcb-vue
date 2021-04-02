ac5257af5392e4e952b7f6f63f04f5eb68aa679c
qinqiang <qinqiang@shoudaozi.com>
2021-04-02 10:49:03 +0800

    envId

--- src/tcb.ts
+++ src/tcb.ts
@@ -1,7 +1,7 @@
 import tcb from "tcb-js-sdk";
 
 // 将你的环境 Id 填写到此处
-export const envId = "tcloud-4gcrbmh247bb85e3";
+export const envId = "tcb-9gexqqgc086482f0";
 
 export const getApp = () => {
   const app = tcb.init({
